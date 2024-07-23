ce17e3f5f168e23554848cdc6493dfa48fe2992a
Rox523 <www.feart39@gmail.com>
2024-07-24 01:34:47 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_home_work = 12
+hours_spent =1.5
+Course_name = "Python"
+one_taskprint=(hours_spent /number_home_work)
+print("Курс:",Course_name,",всего задач:",number_home_work,",Затрачено часоов:",hours_spent,"среднее время выполнения",one_taskprint,)
