b8f321fb8655ad1a6ae5072490789102bdf33361
Rox523 <www.feart39@gmail.com>
2024-07-24 10:43:07 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,5 +1,6 @@
 number_home_work = 12
 hours_spent =1.5
 Course_name = "Python"
-one_taskprint=(hours_spent /number_home_work)
-print("Курс:",Course_name,",всего задач:",number_home_work,",Затрачено часоов:",hours_spent,"среднее время выполнения",one_taskprint,)
+time_for_one_task=(hours_spent /number_home_work)
+print("Курс:"+Course_name+",всего задач:"+str(number_home_work)+",Затрачено часоов:"+str(hours_spent)+",среднее время выполнения",time_for_one_task,"часа.")
+
